766da0b4e933b2d23b95537fda8e3b1e60023f85
Max Shen <maxwshen@gmail.com>
2016-06-09 11:40:24 -0400

    generalized a, b

---
+++ src/a_step.py
@@ -0,0 +1,34 @@
+
+
+import _config
+import sys, os, fnmatch, datetime, subprocess
+import numpy as np
+
+from mylib import util
+
+
+# Default params
+DEFAULT_INP_DIR = _config.DATA_DIR + '2016-06-06/'
+NAME = util.get_fn(__file__)
+
+# Functions
+
+
+
+def main(inp_dir, out_dir, run = True):
+  print NAME  
+  util.ensure_dir_exists(out_dir)
+  if not run:
+    print '\tskipped'
+    return out_dir
+
+  # Function calls
+
+  return out_dir
+
+
+if __name__ == '__main__':
+  if len(sys.argv) > 1:
+    main(sys.argv[1], sys.argv[2])
+  else:
+    main(DEFAULT_INP_DIR, _config.OUT_PLACE + NAME + '/')
